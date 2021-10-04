ce2dc5c90c3cfdcaa70197bff36e821e711cbb44
Benjamin Kiesl <benjamin.kiesl@gmail.com>
2021-10-04 18:09:17 +0200

    Fixed buggy test script.

--- test/test.sh
+++ test/test.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 # Run this file from its parent directory
-./ut_tamarin ./test/test_protocol.spthy --config_file=./test/utt_config.json --timeout=10
+./build/bin/uttamarin ./test/test_protocol.spthy --config_file=./test/utt_config.json --timeout=10
 
